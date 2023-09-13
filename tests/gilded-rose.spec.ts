6957a597176b93bbaa9045267c79905a0ba3dfcc
Ach <126157297+Achkey@users.noreply.github.com>
2023-09-13 12:17:23 +0200

    Added all filles from local

--- tests/gilded-rose.spec.ts
+++ tests/gilded-rose.spec.ts
@@ -1,74 +1,58 @@
-import { Item, GildedRose } from '../src/updated';
+import { GildedRose, Item } from '../src/updated'; // Assurez-vous que l'importation de vos classes est correcte.
 
-describe("GildedRose", () => {
-  describe("update de la qualité pour item normal", () => {
-    it("should decrease quality by 1 avant sellIn date", () => {
-      const item = new Item("Normal Item", 5, 10);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(9);
-    });
-
-    it("should decrease quality by 2 après sellIn date", () => {
-      const item = new Item("Normal Item", -1, 10);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(8);
-    });
-
-    it("should not decrease quality below 0", () => {
-      const item = new Item("Normal Item", 5, 0);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(0);
-    });
+describe('Gilded Rose', () => {
+  it('should match snapshot for Conjured item', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 2, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
   });
 
-  describe("update de la qualité pour Aged Brie", () => {
-    it("should increase quality by 1 avant sellIn date", () => {
-      const item = new Item("Aged Brie", 5, 10);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(11);
-    });
-
-    it("should increase quality by 2 après sellIn date", () => {
-      const item = new Item("Aged Brie", -1, 10);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(12);
-    });
-
-    it("should not increase quality above 50", () => {
-      const item = new Item("Aged Brie", 5, 50);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(50);
-    });
+  it('should match snapshot for Aged Brie with sellIn = 0', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
   });
 
-  // tests pour Sulfuras/Backstage passes
-
-  describe("update de la qualité pour Sulfuras", () => {
-    it("should not change quality or sellIn for Sulfuras", () => {
-      const item = new Item("Sulfuras, Hand of Ragnaros", 5, 80);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(80);
-      expect(items[0].sellIn).toBe(5);
-    });
+  it('should match snapshot for Backstage passes with sellIn = 0', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
   });
 
-  describe("update de la qualité pour Backstage Passes", () => {
-    it("should increase quality by 1 avant sellIn date", () => {
-      const item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
-      const gildedRose = new GildedRose([item]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toBe(21);
-    });
-
-    // Tests..
+  it('should match snapshot for Sulfuras with sellIn = 0', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
   });
 
-    // Tests..
+  it('should match snapshot for empty items array', () => {
+    const gildedRose = new GildedRose([]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
+  });
+
+  it('should match snapshot for normal item with sellIn > 0 and quality > 0', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 5, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
+  });
+
+  it('should match snapshot for normal item with sellIn = 0 and quality > 0', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 0, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
+  });
+
+  it('should match snapshot for normal item with sellIn > 0 and quality = 0', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 5, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
+  });
+
+  it('should match snapshot for normal item with sellIn = 0 and quality = 0', () => {
+    const gildedRose = new GildedRose([new Item('Normal Item', 0, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items).toMatchSnapshot();
+  });
+
 });
