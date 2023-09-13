4284acf9a946980889af6daccf33d71ecd5d6bf3
anonems <93661372+anonems@users.noreply.github.com>
2023-09-13 09:15:40 +0200

    last push

--- tests/gilded-rose.spec.ts
+++ tests/gilded-rose.spec.ts
@@ -22,6 +22,13 @@ describe("GildedRose", () => {
       const items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(0);
     });
+
+    it("should not increase quality above 50", () => {
+      const item = new Item("Normal Item", 5, 55);
+      const gildedRose = new GildedRose([item]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+    });
   });
 
   describe("update quality for Aged Brie", () => {
@@ -67,8 +74,40 @@ describe("GildedRose", () => {
       expect(items[0].quality).toBe(21);
     });
 
-    // Tests..
+    it("should increase quality by 1 for Backstage Passes with positive sellIn", () => {
+      const item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10);
+      const gildedRose = new GildedRose([item]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(11);
+    });
+
+    it("should increase quality by 2 when there are 10 days or less left", () => {
+      const item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+      const gildedRose = new GildedRose([item]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(22);
+    });
   });
 
-    // Tests..
+  it("should increase quality by 2 for Aged Brie with negative sellIn", () => {
+    const item = new Item("Aged Brie", -1, 10);
+    const gildedRose = new GildedRose([item]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(12);
+  });
+
+  it("should increase quality by 3 when there are 5 days or less left", () => {
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+    const gildedRose = new GildedRose([item]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
+  });
+
+it("should drop quality to 0 after the concert", () => {
+    const item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+    const gildedRose = new GildedRose([item]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
 });
